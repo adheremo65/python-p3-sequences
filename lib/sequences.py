4c707f784c3f5a26d5eaaf299f5687c2428dde55
Abdela Umer <adheremo65@gmail.com>
2023-07-16 10:48:38 -0400

    fibonacci in python

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+  x = []
+  if length == 0:
+      x = [0,1]
+      print(x)
+  elif length == 1:
+      x = [0,1]
+      print(x)
+  else: 
+      x = [0,1]
+  for i in range(2,length):
+        x.append(x[i-1] + x[i-2])
+  print(x)
+
+        
