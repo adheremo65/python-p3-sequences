9cdc7868dd2622c09aaca010ef2dc7649f53db2c
Abdela Umer <adheremo65@gmail.com>
2023-07-16 10:57:43 -0400

    sequence fibbonaci

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-  x = []
-  if length == 0:
-      x = [0,1]
-      print(x)
-  elif length == 1:
-      x = [0,1]
-      print(x)
-  else: 
-      x = [0,1]
-  for i in range(2,length):
-        x.append(x[i-1] + x[i-2])
-  print(x)
-
-        
+    fib_sequence = [0, 1]  # Initialize the Fibonacci sequence with the first two numbers
+    if length == 0:
+        print([])
+    elif length == 1:
+        print([0])
+    elif length == 2:
+        print(fib_sequence)
+    else:
+        for i in range(2, length):
+            fib_sequence.append(fib_sequence[i - 1] + fib_sequence[i - 2])
+        print(fib_sequence)
